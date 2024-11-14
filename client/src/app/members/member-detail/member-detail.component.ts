f582772b035fc54b1c5291fd5fcb7958e1f19604
tariquldiu <Gh-27928512>
2024-11-15 00:40:13 +0600

    photo gallery

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,11 +3,13 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { MemberService } from 'src/app/_services/member.service';
 import { Member } from 'src/app/_models/member';
 import { ActivatedRoute } from '@angular/router';
+import { NgImageSliderModule } from 'ng-image-slider';
+
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule, NgImageSliderModule],
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css']
 })
@@ -16,6 +18,7 @@ export class MemberDetailComponent implements OnInit {
   private memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   member?:Member;
+  imageObject: Array<object> = [];
 
   ngOnInit(){
     this.loadMember();
@@ -23,13 +26,18 @@ export class MemberDetailComponent implements OnInit {
 
   loadMember()
   {
+
+
     const username = this.route.snapshot.paramMap.get('username');
     if(!username)return;
 
     this.memberService.getMember(username).subscribe({
       next: member =>{
         this.member = member;
-       
+        member.photos.map((p, index) => {
+          this.imageObject.push({image:p.url, thumbImage: p.url, alt: member.knownAs, title: (index +1) + ': ' + member.knownAs})
+        }
+        )
       } 
     })
       
