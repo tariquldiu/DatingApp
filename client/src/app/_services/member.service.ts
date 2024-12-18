acdcb0cd91759e7fead852631e87a5d7283e92f6
tariquldiu <Gh-27928512>
2024-12-19 00:08:29 +0600

    end of the section 10

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { AccountService } from './account.service';
 import { Member } from '../_models/member';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,11 +20,18 @@ export class MemberService {
   }
 
   getMember(username: string){
+    const member = this.members().find(x=>x.userName === username);
+    if(member !== undefined) return of(member);
+    
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member){
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      tap(()=>{
+        this.members.update(members=> members.map(m=>m.userName === member.userName? member: m))
+      })
+    )
   }
  
 }
