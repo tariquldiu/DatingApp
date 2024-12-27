6bc344fea06ff79497376246618c468365ee31b8
tariquldiu <Gh-27928512>
2024-12-27 19:52:03 +0600

    File upload design

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -57,4 +57,8 @@ export class MemberEditComponent implements OnInit{
     })
     
   }
+
+  onMemberChange(event: Member){
+    this.member = event
+  }
 }
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,14 +1,56 @@
-import { Component, Input} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, inject, Input, OnInit, Output} from '@angular/core';
+import { CommonModule, DecimalPipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { Member } from 'src/app/_models/member';
+import { FileUploader, FileUploadModule } from 'ng2-file-upload';
+import { environment } from 'src/environments/environment';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
   selector: 'app-photo-editor',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgIf, NgFor, NgStyle, NgClass, FileUploadModule, DecimalPipe],
   templateUrl: './photo-editor.component.html',
   styleUrls: ['./photo-editor.component.css']
 })
-export class PhotoEditorComponent {
+export class PhotoEditorComponent implements OnInit{
+  private accountService = inject(AccountService);
   @Input() member!: Member; 
+  @Output() memberChange = new EventEmitter<Member>(); 
+  uploader?: FileUploader
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+
+  ngOnInit(): void{
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any){
+   this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader(){
+    this.uploader = new FileUploader({
+        url: this.baseUrl + 'users/add-photo',
+        authToken: 'Bearer ' + this.accountService.currentUser()?.token,
+        isHTML5:true,
+        allowedFileType: ['image'],
+        removeAfterUpload: true,
+        autoUpload: false,
+        maxFileSize: 10 * 1024 * 1024
+
+    });
+
+
+    this.uploader.onAfterAddingFile = (file) =>{
+      file.withCredentials = true;
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, header) =>{
+         const photo = JSON.parse(response);
+         const updatedMember = {...this.member}
+         updatedMember.photos.push(photo);
+         this.memberChange.emit(updatedMember);
+    }
+  }
+
 }
